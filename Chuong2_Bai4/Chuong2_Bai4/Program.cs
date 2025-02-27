17700d78c1a9fbf9e2886784ac69ea588cc86c7b
quankudo <quannguyen2004aa@gmail.com>
2025-02-27 18:23:12 +0700

    Complete Bai4 Chuong2 Array use Foreach

--- Chuong2_Bai4/Chuong2_Bai4/Program.cs
+++ Chuong2_Bai4/Chuong2_Bai4/Program.cs
@@ -10,6 +10,17 @@ namespace Chuong2_Bai4
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Khai báo mảng tên sinh viên
+            string[] sinhViens_332 = { "An", "Bình", "Cường", "Dương", "Hải" };
+
+            // Duyệt mảng bằng foreach để in danh sách sinh viên
+            Console.WriteLine("Danh sách sinh viên:");
+            foreach (string ten_332 in sinhViens_332)
+            {
+                Console.WriteLine($"- {ten_332}");
+            }
+            Console.ReadKey();
         }
     }
 }
