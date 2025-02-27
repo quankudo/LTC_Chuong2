832d1c5d228eb68b9e6ac80d067fed6f1317b1fc
quankudo <quannguyen2004aa@gmail.com>
2025-02-27 18:02:20 +0700

    Complete Bai1 Chuong2 optional parameter

--- Chuong2_Bai1/Chuong2_Bai1/Program.cs
+++ Chuong2_Bai1/Chuong2_Bai1/Program.cs
@@ -8,8 +8,19 @@ namespace Chuong2_Bai1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int TinhTong(int a_332, int b_332 = 10)
         {
+            return a_332 + b_332;
+        }
+
+        static void Main()
+        {
+            // Đặt encoding UTF-8
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Tổng của 5 và 7: " + TinhTong(5, 7));
+            Console.WriteLine("Tổng của 5 và giá trị mặc định 10: " + TinhTong(5));
+            Console.ReadKey();
         }
     }
 }
